295a5b249293dbe2a69a0d32b3fb2cc5f4bbe2b3
ivanrlmx99 <ivanlaureano77@gmail.com>
2020-10-19 16:14:24 -0500

    Proyecto completo de spring con jpa y my sql

---
+++ src/main/java/com/bolsadeideas/springboot/app/SpringJpaMysqlCrudApplication.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaMysqlCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaMysqlCrudApplication.class, args);
+	}
+
+}
