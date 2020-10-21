373aee134239e8738ba7b84fad30be3231f6b9f6
ivanrlmx99 <ivanlaureano77@gmail.com>
2020-10-21 00:38:35 -0500

    paginador y clase util

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -5,17 +5,25 @@ import java.util.Map;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
+
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 import com.bolsadeideas.springboot.app.models.service.IClienteService;
+import com.bolsadeideas.springboot.app.util.paginator.PageRender;
+
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @SessionAttributes("cliente")
@@ -25,9 +33,14 @@ public class ClienteController {
 	private IClienteService clienteService;
 
 	@RequestMapping(value = "/listar", method = RequestMethod.GET)
-	public String listar(Model model) {
+	public String listar(@RequestParam(name="page" ,defaultValue="0")int page, Model model) {
+		Pageable pageRequest=PageRequest.of(page, 4);
+		
+		Page<Cliente>clientes=clienteService.findAll(pageRequest);
+		PageRender<Cliente> pageRender = new PageRender<Cliente>("/listar", clientes);
 		model.addAttribute("titulo", "Listado de clientes");
-		model.addAttribute("clientes", clienteService.findAll());
+		model.addAttribute("page", pageRender);
+		model.addAttribute("clientes", clientes);
 		return "listar";
 	}
 	
@@ -41,13 +54,20 @@ public class ClienteController {
 	}
 	
 	@RequestMapping(value="/form/{id}")
-	public String editar(@PathVariable(value="id") Long id, Map<String, Object> model) {
+	public String editar(@PathVariable(value="id") Long id, Map<String, Object> model,RedirectAttributes flash) {
 		
 		Cliente cliente = null;
 		
 		if(id > 0) {
 			cliente = clienteService.findOne(id);
+			
+			if(cliente==null) {
+				flash.addAttribute("error", "Cliente no existe en la base de datos");
+				return "redirect:/listar";
+			}
+			
 		} else {
+			flash.addFlashAttribute("error", "Cliente no puede ser 0");
 			return "redirect:/listar";
 		}
 		model.put("cliente", cliente);
@@ -56,22 +76,26 @@ public class ClienteController {
 	}
 	
 	@RequestMapping(value = "/form", method = RequestMethod.POST)
-	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model,RedirectAttributes flash, SessionStatus status) {
 		if(result.hasErrors()) {
 			model.addAttribute("titulo", "Formulario de Cliente");
 			return "form";
 		}
+		String mensajeFlash=(cliente.getId()!=null)?"Cliente editado con éxito":"Cliente creado con éxito";
 		
 		clienteService.save(cliente);
+		flash.addFlashAttribute("success", mensajeFlash);
+		System.out.println("CLIENTE CREADO EXITOSAMENTEEEEE");
 		status.setComplete();
 		return "redirect:listar";
 	}
 	
 	@RequestMapping(value="/eliminar/{id}")
-	public String eliminar(@PathVariable(value="id") Long id) {
+	public String eliminar(@PathVariable(value="id") Long id,RedirectAttributes flash) {
 		
 		if(id > 0) {
 			clienteService.delete(id);
+			flash.addFlashAttribute("error", "Cliente eliminado exitosamente");
 		}
 		return "redirect:/listar";
 	}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,3 +7,19 @@ INSERT INTO CLIENTES (id, nombre, apellido, email, create_at) values(101,'Iveth'
 INSERT INTO CLIENTES (id, nombre, apellido, email, create_at) values(109,'Ivan','ramos','ivanmx99@gmail.com','2020-07-13');
 INSERT INTO CLIENTES (id, nombre, apellido, email, create_at) values(103,'Angelito','lopez','endejelo@gmail.com','2020-10-13');
 INSERT INTO CLIENTES (id, nombre, apellido, email, create_at) values(77,'Brittany','ramos','itza@gmail.com','2020-10-13');
+INSERT INTO CLIENTES (id, nombre, apellido, email, create_at) values(10,'Mariana','Diaz','mary@gmail.com','2020-10-13');
+INSERT INTO CLIENTES (id, nombre, apellido, email, create_at) values(21,'Matias','medinaz','maty@gmail.com','2020-10-13');
+INSERT INTO CLIENTES (id, nombre, apellido, email, create_at) values(3,'Malisa','basurto','mely@gmail.com','2020-10-13');
+INSERT INTO CLIENTES (id, nombre, apellido, email, create_at) values(4,'Mirella','quintero','mary@gmail.com','2020-10-13');
+INSERT INTO CLIENTES (id, nombre, apellido, email, create_at) values(5,'lucila','laureano','luci@gmail.com','2020-10-13');
+INSERT INTO CLIENTES (id, nombre, apellido, email, create_at) values(6,'Hugo','boss','hugo@gmail.com','2020-10-13');
+INSERT INTO CLIENTES (id, nombre, apellido, email, create_at) values(7,'roberto','carlos','rober@gmail.com','2020-10-13');
+INSERT INTO CLIENTES (id, nombre, apellido, email, create_at) values(8,'kiara','quesadilla','dilla@gmail.com','2020-10-13');
+INSERT INTO CLIENTES (id, nombre, apellido, email, create_at) values(9,'Cenon','tostadas','toty@gmail.com','2020-10-13');
+INSERT INTO CLIENTES (id, nombre, apellido, email, create_at) values(10,'neftali','ramos','mary@gmail.com','2020-10-13');
+INSERT INTO CLIENTES (id, nombre, apellido, email, create_at) values(11,'Mariana','Diaz','mary@gmail.com','2020-10-13');
+INSERT INTO CLIENTES (id, nombre, apellido, email, create_at) values(12,'flavio','rodriguez','flavelo@gmail.com','2020-10-13');
+INSERT INTO CLIENTES (id, nombre, apellido, email, create_at) values(13,'Karla','reynosa','cary@gmail.com','2020-10-13');
+INSERT INTO CLIENTES (id, nombre, apellido, email, create_at) values(14,'Martina','alarcon','marty@gmail.com','2020-10-13');
+INSERT INTO CLIENTES (id, nombre, apellido, email, create_at) values(15,'Mariana','Diaz','mary@gmail.com','2020-10-13');
+
