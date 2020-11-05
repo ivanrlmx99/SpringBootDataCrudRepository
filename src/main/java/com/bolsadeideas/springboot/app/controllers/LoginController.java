359bd460654adde9d4a904194f101655b7705fa8
ivanrlmx99 <ivanlaureano77@gmail.com>
2020-11-05 01:12:50 -0600

    Se puso notificacion de erro en login

--- src/main/java/com/bolsadeideas/springboot/app/controllers/LoginController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/LoginController.java
@@ -5,13 +5,14 @@ import java.security.Principal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
 	
 	@GetMapping("/login")
-	public String login(Model model,Principal principal,RedirectAttributes flash) {
+	public String login(@RequestParam(value="error",required = false)String error,Model model,Principal principal,RedirectAttributes flash) {
 		
 		if(principal!=null) {
 			flash.addFlashAttribute("info","Sesión ya iniciada");
@@ -19,7 +20,11 @@ public class LoginController {
 			return"redirect:/";
 		}
 		
-		return"login";
+		if(error!=null) {
+		model.addAttribute("error","Contraseña o nombre usuario incorrecto!");
+		}
+		
+		return"loginy";
 		
 	}
 
